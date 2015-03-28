7d8b0c244cdf7ea624a2c25f334057a2d9c4afc7
Lance <lance@reindeercompany.com>
2015-03-28 13:07:27 -0400

    DB change

--- application/config/database.php
+++ application/config/database.php
@@ -54,13 +54,13 @@ $db['default']['username'] = 'root';
 $db['default']['password'] = 'root';
 $db['default']['database'] = 'webtinder';
 
-/*
+
 // Remote settings
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'lance';
 $db['default']['password'] = 'Codecall87!!';
 $db['default']['database'] = 'wetinder';
-*/
+
 
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
