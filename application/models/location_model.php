cd34ea7ce73d9c7f9a35c1191ae81b8ae997ec4d
Lance <lance@reindeercompany.com>
2015-04-13 14:55:27 -0400

    Footer change

--- application/models/location_model.php
+++ application/models/location_model.php
@@ -232,7 +232,7 @@
 					WHERE id > ?
 					GROUP BY state
 					LIMIT 5";
-			$query = $this->db->query($sql, array(mt_rand(95867, 125976)));
+			$query = $this->db->query($sql, array(mt_rand(95867, 125000)));
 			$count = $query->num_rows();
 			$i = 0;
 
