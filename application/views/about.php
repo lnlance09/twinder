026e7daa18d63f16601c4f6bbfd573d7d67210f1
Lance <lance@reindeercompany.com>
2015-04-19 07:42:32 -0400

    Updated the about page

--- application/views/about.php
+++ application/views/about.php
@@ -35,6 +35,36 @@
                     It has always seemed a bit strange that Tinder's <a href="http://gotinder.com" target="_blank" itemprop="sameAs">website</a> was nothing more than just a few static pages.
                     Seriously. Their website doesn't have any of the functionality that the app does. 
                     It's really just a website that tells you to go download the app on your phone.
+                    So, I decided to introduce three new and considerable improvements to Tinder.
+                </p>
+
+                <ol>
+                    <li>
+                        <b>Search:</b> The random nature of discovering new users served Tinder really well in its early days, but I think it's gotten to the stage where Tinder users need more outlets to express themsevles.
+                        At the current moment, there isn't any way to search for users on Tinder based upon location unless you have a paid subscription (Tinder Plus Passport). 
+                        But, with Twinder, you can. In addition to being able to execute location based user searches, you can search for users based upon their names.
+                        Of course, just like Tinder, search results can also be filtered by age, gender and distance.
+                        I really think this makes Tinder a more open community.
+                    </li>
+
+                    <li>
+                        <b>Profile:</b> Now that Twinder has aquired an abundance of information about Tinder users located throughout America, the goal is to build an audience.
+                        Each and every Tinder user that Twinder has fetched automatically gets their own dedicated profile page on Twinder.
+                        The Twinder profile has all of what the Tinder profile has and more.
+                        Most importantly, the idea of Tinder users having their own URL makes pinpointing given users much easier.
+                    </li>
+
+                    <li>
+                        <b>Voting:</b> As you browse and search for profiles on Twinder, you'll see that you're presented with the option of voting 'hot' or 'not.' 
+                        I'm curious to see which users are rated the most attractive. 
+                        Sort of like a Miss America beauty pageant for Tinder users. Pretty cool...
+                    </li>
+                </ol>
+
+                <p>
+                    This is just sort of an experimental idea that I decided to do in my free time. 
+                    The simplest way to describe Twinder is by just calling it 'Tinder for Web.'
+                    Tinder, but just for a different platform. I hope you all enjoy using this. Thanks.
                 </p>
             </div>
 
