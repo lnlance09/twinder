0142d309ffefea9743020a2bd9d671529836a28a
Lance <lance@reindeercompany.com>
2015-05-10 00:06:58 -0400

    Analytics bug

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -184,7 +184,6 @@
         })(window,document,'script','dataLayer','GTM-THN42S');
     </script>
 
-    <!-- StatCounter
     <script>
         var sc_project = 10187180; 
         var sc_invisible = 1; 
@@ -192,7 +191,6 @@
         var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
         document.write("<sc"+"ript type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js'></"+"script>");
     </script>
-    -->
 <?php
     }
 ?>
