3bb4a90380ed1ba4ef82d617c3b75a56f47c3f63
Lance <lance@reindeercompany.com>
2015-05-10 14:05:51 -0400

    Places autocomplete restyle

--- application/views/autocomplete/places.php
+++ application/views/autocomplete/places.php
@@ -7,8 +7,6 @@
 ?>
 		<li class="list-group-item" city="<?php echo $places[$i]['city']; ?>" state="<?php echo $places[$i]['state']; ?>">
 			<?php echo $places[$i]['city'].', '.$places[$i]['state']; ?>
-			<span class="badge badge-danger pull-right"><?php echo number_format($places[$i]['count']); ?></span>
-			<span class="clearfix"></span>
 		</li>
 <?php
 		}
