321f5c5763c5ae42e8a3602e37034e5c2e0bca3f
Lance <lance@reindeercompany.com>
2015-05-10 00:04:52 -0400

    Analytics bug

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -171,8 +171,20 @@
 
     if($base_url != '/twinder/') {
 ?>
-    <!-- StatCounter -->
-    <!--
+    <!-- Google Tag Manager -->
+    <noscript>
+        <iframe src="//www.googletagmanager.com/ns.html?id=GTM-THN42S" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-THN42S');
+    </script>
+
+    <!-- StatCounter
     <script>
         var sc_project = 10187180; 
         var sc_invisible = 1; 
