2b35a50b5c394e6bd9e3dd89db411bfd9dcac13e
Lance <lance@reindeercompany.com>
2015-07-12 11:42:12 -0400

    Removed popover from user.js

--- public/js/users.js
+++ public/js/users.js
@@ -9,9 +9,6 @@ $(document).ready(function() {
     var can_like = $('#like').text();
     var styles = [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#aadd55"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#0099dd"}]}];
 
-    // Popover for stats
-    $('.mypopover').popover();
-
     // Google Maps
     function Initialize(lat, lon) {
         var distance = $('#radius').text();
